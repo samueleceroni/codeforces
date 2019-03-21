fc17cfdc7e53ce47bea83abb6e00ba76c00e170b
Samuele Ceroni <samuele.ceroni@gmail.com>
2019-03-21 18:08:31 +0100

    contest 539

---
+++ contests/539/A/main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+
+#define INF 1e9
+
+using namespace std;
+
+typedef pair<int,int> ii;
+typedef vector<int> vi;
+typedef vector<vector<ii> > vvii;
+
+int main(){
+
+
+}
